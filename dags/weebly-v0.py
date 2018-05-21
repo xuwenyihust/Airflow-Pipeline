183ff12b4dd67e7c08e1bd98567aa284d05f5989
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-21 17:34:15 -0500

    Added dag weebly-v0.

---
+++ dags/weebly-v0.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.python_operator import PythonOperator
+from ..src.get_customer_summary import get_customer_summary
+
+current_date = datetime.now()
+
+default_args = {
+                'owner': 'airflow',
+                'depends_on_past': False,
+                'start_date': datetime(current_date.year, current_date.month, current_date.date, current_date.hour, current_date.minute),
+                'email': ['airflow@example.com'],
+                'email_on_failure': False,
+                'email_on_retry': False,
+                'retries': 1,
+                'retry_delay': timedelta(minutes=5)
+                }
+
+dag = DAG('weebly-v0', schedule_interval="@once", catchup=False, default_args=default_args)
+
+task_get_customer_summary = PythonOperator(task_id='get_customer_summary',
+                                           python_callable=get_customer_summary,
+                                           provide_context=False,
+                                           dag=dag)
