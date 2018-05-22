27b21778f4a553fe4ddc1ea2a6b4cf6f25a4a6c4
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-22 11:07:31 -0500

    Updated main logic to aggregate by country.

--- src/get_customer_summary.py
+++ src/get_customer_summary.py
@@ -7,7 +7,7 @@ def get_customer_summary():
     f_in = open(input_path)
     f_out = open(output_path, "a+")
 
-    customer_dict = dict()
+    geography_dict = dict()
 
     for line in f_in.readlines():
         # Exclude head
@@ -18,14 +18,14 @@ def get_customer_summary():
             except:
                 pass
 
-            if customerid not in customer_dict:
-                customer_dict[customerid] = 1
+            if country not in geography_dict:
+                geography_dict[country] = 1
             else:
-                customer_dict[customerid] += 1
+                geography_dict[country] += 1
 
     f_in.close()
 
-    for customerid, count in customer_dict.items():
-        f_out.write(str(customerid)+","+str(count)+"\n")
+    for country, count in customer_dict.items():
+        f_out.write(str(country)+","+str(count)+"\n")
 
     f_out.close()
