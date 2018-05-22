0c73f38019a145bed5a97f4c4f8eb3111cff79c2
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-22 11:34:06 -0500

    Renamed get_customer_summary.py to get_customer_geo_dist.py.

---
+++ src/get_customer_geo_dist.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import os
+
+def get_customer_geo_dist():
+    input_path = os.path.join(os.path.dirname(__file__), '../data/weebly/in/customer_info.csv')
+    output_path = os.path.join(os.path.dirname(__file__), '../data/weebly/out/customer_summary.csv')
+
+    customer_df = pd.read_csv(input_path)
+
+    # Group by country and sum
+    geo_dist_df = customer_df.goupby(['country']) \
+                             .sum() \
+                             .rename(columns={0:"country",1:"count"})
+
+    geo_dist_df.to_csv(output_path)
