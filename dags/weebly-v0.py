7eefebb2ffa75cace4da24248dfe31d3b0925137
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-21 20:12:44 -0500

    Added new dummy task.

--- dags/weebly-v0.py
+++ dags/weebly-v0.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from datetime import datetime, timedelta
+from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from src.get_customer_summary import get_customer_summary
 
@@ -22,7 +23,13 @@ default_args = {
 
 dag = DAG('weebly-v0', schedule_interval="@once", catchup=False, default_args=default_args)
 
+task_get_date = BashOperator(task_id='get_date',
+                             bash_command='date',
+                             dag=dag)
+
 task_get_customer_summary = PythonOperator(task_id='get_customer_summary',
                                            python_callable=get_customer_summary,
                                            provide_context=False,
                                            dag=dag)
+
+task_get_customer_summary.set_upstream(task_get_date)
