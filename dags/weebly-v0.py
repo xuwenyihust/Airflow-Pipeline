dcfbc18dd29d83338c093093862653028d7fd001
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-21 20:38:23 -0500

    Updated dag args.

--- dags/weebly-v0.py
+++ dags/weebly-v0.py
@@ -3,22 +3,34 @@ from datetime import datetime, timedelta
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from src.get_customer_summary import get_customer_summary
+import os
 
 current_date = datetime.utcnow() - timedelta(days=1)
+
+# default_args = {
+#                 'owner': 'airflow',
+#                 'depends_on_past': False,
+#                 'start_date': datetime(int(current_date.year), \
+#                                        int(current_date.month), \
+#                                        int(current_date.day), \
+#                                        int(current_date.hour), \
+#                                        int(current_date.minute)),
+#                 'email': ['airflow@example.com'],
+#                 'email_on_failure': False,
+#                 'email_on_retry': False,
+#                 'retries': 1,
+#                 'retry_delay': timedelta(minutes=5)
+#                 }
 
 default_args = {
                 'owner': 'airflow',
                 'depends_on_past': False,
-                'start_date': datetime(int(current_date.year), \
-                                       int(current_date.month), \
-                                       int(current_date.day), \
-                                       int(current_date.hour), \
-                                       int(current_date.minute)),
+                'start_date': datetime(2018, 5, 15),
                 'email': ['airflow@example.com'],
                 'email_on_failure': False,
                 'email_on_retry': False,
                 'retries': 1,
-                'retry_delay': timedelta(minutes=5)
+                'retry_delay': timedelta(minutes=5),
                 }
 
 weebly_pipeline = DAG('weebly-v0', schedule_interval="@once", catchup=False, default_args=default_args)
