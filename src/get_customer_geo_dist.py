5561da0a91419103276b22f1d9e0bb460bfe1e82
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-22 12:52:55 -0500

    Added src/ to PYTHONPATH.

--- src/get_customer_geo_dist.py
+++ src/get_customer_geo_dist.py
@@ -2,15 +2,15 @@ import pandas as pd
 import numpy as np
 import os
 
-def get_customer_geo_dist(input_file):
+def get_customer_geo_dist(input_file, output_file):
     input_path = input_file
-    output_path = os.path.join(os.path.dirname(__file__), '../data/weebly/out/customer_summary.csv')
+    output_path = output_file
 
     customer_df = pd.read_csv(input_path)
 
     # Group by country and sum
     geo_dist_df = customer_df.groupby(['country']) \
-                             .sum() \
+                             .count() \
                              .rename(columns={0:"country",1:"count"})
 
     geo_dist_df.to_csv(output_path)
