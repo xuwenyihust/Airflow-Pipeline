36b6a29173b0f5594deaecf69371531543781852
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-24 12:33:20 -0500

    Added some comments.

--- tests/test_get_customer_consum.py
+++ tests/test_get_customer_consum.py
@@ -8,8 +8,8 @@ def test_customer_consum(tmpdir):
     product_file = tmpdir.join('dummy_product_info.csv')
     output_file = tmpdir.join('dummy_customer_consum.csv')
 
+    # Dummy data
     invoice_file.write("invoiceno,stockcode,quantity,invoicedate,customerid\n537127,85123A,128,2010-12-05 12:13:00.000000,13831")
-
     product_file.write("stockcode,description,unitprice\n85123A,WHITE HANGING HEART T-LIGHT HOLDER,3.24")
 
     get_customer_consum(invoice_file.strpath, product_file.strpath, output_file.strpath)
